390d982e7fdd94888e8586603e9752d3c44738b6
spectatorMrZ <zhuchunan001@sina.com>
2021-03-22 17:14:07 +0800

    Initial Commit

---
+++ ZFPlayerDemo-SwiftUI/ZFPlayerDemo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZFPlayerDemo_SwiftUIApp.swift
+//  ZFPlayerDemo-SwiftUI
+//
+//  Created by 楠 on 2021/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ZFPlayerDemo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
